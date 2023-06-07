bf852d6f1da47caab6a735d54b370cd04bdcf58e
Andrey <andreyvoropaev1314@gmail.com>
2023-06-07 07:27:52 +0300

    add model for inventory

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import axios from "axios";
 
 class App extends React.Component {
 
-    baseUrl = 'http://localhost:5000/api/'
+    baseUrl = 'https://localhost:7070/api/'
     constructor(props) {
         super(props);
 
@@ -20,31 +20,17 @@ class App extends React.Component {
                 userLogin: "SeenceX",
                 userInventoryId: 0,
             },
-            Inventory: [
-                {
-                    itemId: 0,
-                    itemImg: "https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFU4naLOJzgUuYqyzIaIxa6jMOLXxGkHvcMjibmU99Sg3Qaw-hA_ZWrzLISLMlhpgJJUhGE/64fx64f",
-                    itemName: "Snakebite case",
-                    itemCount: 10,
-                    itemInitPrice: 10,
-                    itemCurrentPrice: 20,
-                },
-                {
-                    itemId: 1,
-                    itemImg: "https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFU4naLOJzgUuYqyzIaIxa6jMOLXxGkHvcMjibmU99Sg3Qaw-hA_ZWrzLISLMlhpgJJUhGE/64fx64f",
-                    itemName: "Bravo case",
-                    itemCount: 1,
-                    itemInitPrice: 10,
-                    itemCurrentPrice: 1,
-                },
-            ]
+            Inventory: []
         }
-
         this.isLoginned = this.isLoginned.bind(this);
         this.getItemProfit = this.getItemProfit.bind(this);
         this.LoadInventory = this.LoadInventory.bind(this)
-        //this.LoadInventory()
+        //this.LoadInventory(1);
     }
+    componentDidMount() {
+        this.getAllUsers();
+    }
+
     render() {
         return (
             <Router>
@@ -52,31 +38,33 @@ class App extends React.Component {
                 <Routes>
                     <Route path="*" element={<NotFound/>}/>
                     <Route exact path="/" element={<HomePage/>}/>
-                    <Route path="/inventory" element={<InventoryPage getItemProfit={this.getItemProfit} Inventory={this.state.Inventory}/>}/>
+                    <Route path="/inventory" element={<InventoryPage getItemProfit={this.getItemProfit} LoadInventory={this.LoadInventory} Inventory={this.state.Inventory}/>}/>
                 </Routes>
             </Router>
         )
     }
 
-    LoadInventory(inventoryId){
-        axios.get(this.baseUrl+'/inventory',inventoryId).then(res => {
-            var userInventory = [];
-            res.data.forEach(item => {
-                userInventory.push({
-                    itemId: item.id,
-                    itemImg: item.img,
-                    itemName: item.name,
-                    itemCount: item.count,
-                    itemInitPrice: item.initPrice,
-                    itemCurrentPrice: item.currPrice,
-                })
-            });
-            this.setState({
-                Inventory: []
-            }, () => this.setState({
-                Inventory: userInventory
-            }))
-        })
+    async LoadInventory(userId){
+        try {
+            console.log(1);
+            const res = await axios.get(this.baseUrl + 'Inventory/' + userId);
+            const userInventory = res.data.map(item => ({
+                itemId: item.itemid,
+                itemImg: item.itemImg,
+                itemName: item.itemName,
+                itemCount: item.itemCount,
+                itemInitPrice: item.initialPrice,
+                itemCurrentPrice: item.currentPrice,
+                itemQuality: item.itemQuality,
+                itemRare: item.itemRare,
+                itemType: item.itemType,
+                itemCollection: item.itemCollection
+            }));
+            this.setState({ Inventory: userInventory });
+        } catch (error) {
+            console.log(error);
+            // обработка ошибки
+        }
 
     }
 
@@ -91,6 +79,20 @@ class App extends React.Component {
             return `+${profit}`
         }
         return `${profit}`
+    }
+
+    getAllUsers(){
+        axios.get(this.baseUrl+"Users/").then(res =>{
+            var users = [];
+            res.data.forEach(item=>{
+                users.push({
+                    Id: item.id,
+                    Login: item.login,
+                    Password: item.password
+                })
+            });
+            console.log(users);
+        })
     }
 }
 
--- src/components/InventoryPage.js
+++ src/components/InventoryPage.js
@@ -3,6 +3,10 @@ import '../css/inventory.css'
 import InventoryTable from "./InventoryPageComponents/InventoryTable";
 
 class InventoryPage extends React.Component{
+
+    componentDidMount() {
+        this.props.LoadInventory(1);
+    }
     render() {
         return(
             <div className="container">
