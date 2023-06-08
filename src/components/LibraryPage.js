4ab5eb1090dcc32ecb438e996f147576798abac2
Andrey <andreyvoropaev1314@gmail.com>
2023-06-08 16:45:02 +0300

    LibraryPage

--- src/components/LibraryPage.js
+++ src/components/LibraryPage.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
 
 class LibraryPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: []
+        };
+    }
+
+    async componentDidMount() {
+        const data = await this.props.allItems();
+        this.setState({ items: data });
+    }
+
     render() {
         return (
             <div>
-
+                <ul>
+                    {this.state.items.map((item, index) => (
+                        <li key={index}>
+                            <img src={item.itemImg} alt={item.itemName} />
+                            <span>{item.itemName}</span>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
